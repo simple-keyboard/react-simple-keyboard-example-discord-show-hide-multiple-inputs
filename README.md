e5e38831eeabd0cae97ed8ff431d7ca5d6677a4b
simple-keyboard <46766174+simple-keyboard@users.noreply.github.com>
2019-01-31 18:46:39 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# react-simple-keyboard-example-discord-show-hide-multiple-inputs
-Created with CodeSandbox
+<p><img src="https://franciscohodge.com/project-pages/simple-keyboard/images/react.png" width="60" align="center" /></p>
+<a href="https://simple-keyboard.com/demo" title="View Demo" target="_blank"><img src="https://franciscohodge.com/project-pages/simple-keyboard/images/reactsimplekeyboard-banner3.png" align="center" width="100%"></a>
+
+  <p>
+  <blockquote><strong>This is a repository for simple-keyboard's React demos</strong> <br /> https://virtual-keyboard.js.org/react</blockquote>
+   </p>
+   <br />
+
+## 📦 Get started with simple-keyboard
+
+Check out the [Getting Started](https://simple-keyboard.com/react/getting-started) docs to begin.
+
+## 📖 Documentation
+Check out the [simple-keyboard documentation](https://simple-keyboard.com/react/documentation) site.
+
+Feel free to browse the [Q&A / Use-cases](https://simple-keyboard.com/react/qa-use-cases/) page for advanced use-cases.
+
+## 🚀 Demo
+
+[https://simple-keyboard.com/demo](https://simple-keyboard.com/demo)
+
+
+### To run the React demos on your own computer
+
+* Clone this repository
+* `npm install`
+* `npm start`
+* Visit [http://localhost:3000/](http://localhost:3000/)
+
+### All simple-keyboard versions
+
+* [Vanilla](https://virtual-keyboard.js.org)
+* [React.js](https://virtual-keyboard.js.org/react)
+* [Angular](https://virtual-keyboard.js.org/angular)
+* [Vue.js](https://virtual-keyboard.js.org/vuejs)
+
+### Questions?
+
+<a href="http://franciscohodge.com/simple-keyboard/chat/join" title="Join our Discord chat" target="_blank"><img src="https://franciscohodge.com/project-pages/simple-keyboard/images/discord.png" align="center" width="200"></a>
+
+## ✅ Contributing
+
+PR's and issues are welcome. Feel free to submit any issues you have at:
+[https://github.com/hodgef/react-simple-keyboard/issues](https://github.com/hodgef/react-simple-keyboard/issues)
---
+++ src/index.js
@@ -0,0 +1,169 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Keyboard from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
+import "./index.css";
+
+class App extends Component {
+  state = {
+    layoutName: "ip",
+    inputName: "input1",
+    input: {}
+  };
+
+  onChangeAll = inputObj => {
+    this.setState({
+      input: inputObj
+    });
+
+    console.log("Input changed", inputObj);
+  };
+
+  onKeyPress = button => {
+    console.log("Button pressed", button);
+
+    /**
+     * If you want to handle the shift and caps lock buttons
+     */
+    if (button === "{shift}" || button === "{lock}") this.handleShift();
+
+    if (button === "{clear}") this.clearScreen();
+  };
+
+  handleShift = () => {
+    let layoutName = this.state.layoutName;
+
+    this.setState({
+      layoutName: layoutName === "default" ? "shift" : "default"
+    });
+  };
+
+  onChangeInput = event => {
+    let inputVal = event.target.value;
+
+    let updatedInputObj = {
+      ...this.state.input,
+      [this.state.inputName]: inputVal
+    };
+
+    this.setState(
+      {
+        input: updatedInputObj
+      },
+      () => {
+        this.keyboard.setInput(inputVal);
+      }
+    );
+  };
+
+  setActiveInput = inputName => {
+    this.setState(
+      {
+        inputName: inputName,
+        keyboardOpen: true
+      },
+      () => {
+        console.log("Active input", inputName);
+      }
+    );
+  };
+
+  closeKeyboard = () => {
+    this.setState({
+      keyboardOpen: false
+    });
+  };
+
+  submit = () => {
+    console.log(this.state.input);
+  };
+
+  clearScreen = () => {
+    let input = { ...this.state.input };
+    let inputName = this.state.inputName;
+    input[inputName] = "";
+
+    this.setState({ input }, () => {
+      this.keyboard.clearInput(inputName);
+      console.log(
+        "cleared",
+        input,
+        this.keyboard.keyboard.options.inputName,
+        this.keyboard.keyboard.input,
+        this.keyboard.getInput()
+      );
+    });
+  };
+
+  render() {
+    let { input, keyboardOpen } = this.state;
+
+    return (
+      <div>
+        <div className="inputsContainer">
+          <input
+            onFocus={() => this.setActiveInput("input1")}
+            value={input["input1"] || ""}
+            placeholder={"Input 1"}
+            onChange={e => this.onChangeInput(e)}
+          />
+          <input
+            onFocus={() => this.setActiveInput("input2")}
+            value={input["input2"] || ""}
+            placeholder={"Input 2"}
+            onChange={e => this.onChangeInput(e)}
+          />
+          <input
+            onFocus={() => this.setActiveInput("input3")}
+            value={input["input3"] || ""}
+            placeholder={"Input 3"}
+            onChange={e => this.onChangeInput(e)}
+          />
+          <input
+            onFocus={() => this.setActiveInput("input4")}
+            value={input["input4"] || ""}
+            placeholder={"Input 4"}
+            onChange={e => this.onChangeInput(e)}
+          />
+          <input
+            onFocus={() => this.setActiveInput("input5")}
+            value={input["input5"] || ""}
+            placeholder={"Input 5"}
+            onChange={e => this.onChangeInput(e)}
+          />
+          <input
+            onFocus={() => this.setActiveInput("input6")}
+            value={input["input6"] || ""}
+            placeholder={"Input 6"}
+            onChange={e => this.onChangeInput(e)}
+          />
+        </div>
+        <div className={`keyboardContainer ${!keyboardOpen ? "hidden" : ""}`}>
+          <Keyboard
+            ref={r => (this.keyboard = r)}
+            inputName={this.state.inputName}
+            layoutName={this.state.layoutName}
+            onChangeAll={inputObj => this.onChangeAll(inputObj)}
+            onKeyPress={button => this.onKeyPress(button)}
+            layout={{
+              ip: ["1 2 3", "4 5 6", "7 8 9", ". 0 {clear}", "{bksp} {enter}"]
+            }}
+            display={{
+              "{clear}": "C",
+              "{bksp}": "backspace",
+              "{enter}": "enter"
+            }}
+          />
+          <button className="submitBtn" onClick={this.submit}>
+            Submit
+          </button>
+          <button className="closeBtn" onClick={this.closeKeyboard}>
+            Close Keyboard
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
